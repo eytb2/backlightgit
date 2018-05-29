bf87e273aa3bcc4aa0a75074808d4561c658a0d5
eytb2 <eytb2@qq.com>
2018-05-29 21:56:52 +0800

    add the baidu ocr

--- app/src/main/java/com/lemon/backnightgit/ui/App.java
+++ app/src/main/java/com/lemon/backnightgit/ui/App.java
@@ -1,16 +1,56 @@
 package com.lemon.backnightgit.ui;
 
 import android.app.Application;
+import android.widget.Toast;
+
+import com.baidu.ocr.sdk.OCR;
+import com.baidu.ocr.sdk.OnResultListener;
+import com.baidu.ocr.sdk.exception.OCRError;
+import com.baidu.ocr.sdk.model.AccessToken;
+import com.lemon.backnightgit.Console;
+import com.orhanobut.hawk.Hawk;
+import com.orhanobut.hawk.HawkBuilder;
+import com.orhanobut.hawk.LogLevel;
 
 import cn.jpush.android.api.JPushInterface;
 
 public class App extends Application {
 
+
+
+    public static boolean hasGotToken;
+    //尝试次数
+    public static int times = 0;
+
     @Override
     public void onCreate() {
         super.onCreate();
-
+        initAccessTokenWithAkSk();
         JPushInterface.setDebugMode(true);
         JPushInterface.init(this);
+        Hawk.init(this)
+                .setEncryptionMethod(HawkBuilder.EncryptionMethod.NO_ENCRYPTION)
+                .setStorage(HawkBuilder.newSqliteStorage(this))
+                .setLogLevel(LogLevel.FULL)
+                .build();
+    }
+
+    private void initAccessTokenWithAkSk() {
+        OCR.getInstance().initAccessTokenWithAkSk(new OnResultListener<AccessToken>() {
+            @Override
+            public void onResult(AccessToken result) {
+                hasGotToken = true;
+            }
+
+            @Override
+            public void onError(OCRError error) {
+                error.printStackTrace();
+                if ( times++ < 3 ) {
+                    initAccessTokenWithAkSk();
+                } else {
+                    Toast.makeText(App.this, "AK，SK方式获取token失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, getApplicationContext(), Console.APP_KEY, Console.APP_SECRET);
     }
 }
---
+++ app/src/main/java/com/lemon/backnightgit/utils/LocalStorageKeys.java
@@ -0,0 +1,9 @@
+package com.lemon.backnightgit.utils;
+
+/**
+ * Created by cretin on 16/3/1.
+ */
+public class LocalStorageKeys {
+    public static final String IS_FIRST = "IS_FIRST";
+
+}
